5e899a2d0bcdf216e5f44f761dd24f05e8b24217
Tiara-Diba-Siregar <89350916+Tiara-Diba-Siregar@users.noreply.github.com>
2023-12-04 17:29:22 +0700

    Update SQLQuery2.sql

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -1,1 +1,16 @@
-select * from FactSalesOrder
+CREATE PROCEDURE summary_status_order
+(@StatusID INT) 
+AS
+BEGIN
+	SELECT
+		FSO.OrderID,
+		C.CustomerName,
+		P.ProductName,
+		FSO.Quantity,
+		SO.StatusOrder
+	FROM FactSalesOrder AS FSO
+	JOIN DimCustomer C ON FSO.CustomerID = C.CustomerID
+	JOIN DimProduct P ON FSO.ProductID = P.ProductID
+	JOIN DimStatusOrder SO ON FSO.StatusID = SO.StatusID
+	WHERE SO.StatusID = @StatusID
+END
